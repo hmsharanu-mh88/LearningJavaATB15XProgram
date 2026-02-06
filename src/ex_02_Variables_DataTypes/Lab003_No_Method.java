5705a3cdfc1e231706b4f7a5eea223e513f81248
Administrator <Administrator@WINDOWS-IDJQOB7>
2026-02-06 08:45:47 +0530

    New changes

---
+++ src/ex_02_Variables_DataTypes/Lab003_No_Method.java
@@ -0,0 +1,10 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab003_No_Method
+{
+    public static void main(String[] args)
+    {
+        String str = "I'm in Console";
+        System.out.println("A message: "+ str);
+    }
+}
---
+++ src/ex_02_Variables_DataTypes/Lab004_Comments.java
@@ -0,0 +1,10 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab004_Comments {
+
+    public static void main(String[] args)
+    {
+        //Comments
+        // New Comments
+    }
+}
